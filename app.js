80d8fda08a35daf0b1238738d3c5b67ab7f0f2f6
Elorm Loh <elormloh27@gmail.com>
2026-08-16 10:53:00 -0700

    so i made some one or two changes to complete it

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ menu.addEventListener("click", function () {
   menuLinks.classList.toggle("active")
   
 });
+
+document.querySelectorAll('.video-facade').forEach(facade => {
+  facade.addEventListener('click', function() {
+    
+    const videoUrl = this.getAttribute('data-src'); const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', videoUrl + "?autoplay=1");
+    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('loading', 'lazy'); // iframe also lazy
+  
+    this.parentNode.replaceChild(iframe, this);
+  });
+});
